4db22b5a95e349578c1cb1bcf67edd270574c0d1
haoyueyang <haoyueyang@ucla.edu>
2017-06-19 04:55:04 -0600

    The Updated Version of Game.h

--- Game.h
+++ Game.h
@@ -12,21 +12,24 @@ public:
     void print();
     void take_input(char move);
     
+    void addup();
+    void adddown();
+    void addleft();
+    void addright();
     
     void moveup();
     void movedown();
     void moveleft();
     void moveright();
     
+
     void move();
     
     int max_value();
-    
     void new_entry();
     
     
     void entry_add(int row1, int col1, int row2, int col2);
-    
     bool check_failed();
     
 private:
