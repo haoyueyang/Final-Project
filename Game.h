0109fe47a7750d696b453f7875f9b6b02b852307
haoyueyang <haoyueyang@ucla.edu>
2017-06-19 01:55:15 -0700

    The Complete Version of Game.h

---
+++ Game.h
@@ -0,0 +1,43 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include "matrix.h"
+
+class Game{
+    
+public:
+    Game();
+    
+    void print();
+    void take_input(char move);
+    
+    
+    void moveup();
+    void movedown();
+    void moveleft();
+    void moveright();
+    
+    void move();
+    
+    int max_value();
+    
+    void new_entry();
+    
+    
+    void entry_add(int row1, int col1, int row2, int col2);
+    
+    bool check_failed();
+    
+private:
+    // Parameters
+    Matrix<int, 16> m;
+    int move_;
+    
+    // private functions
+    int rand_entry();
+    
+};
+
+
+#endif
